3e98b85e889df5b89810e1699055a3f064607e46
claesgill <Claes-Jonas.Gill@skatteetaten.no>
2020-10-09 11:35:59 +0200

    Bumped versions minio and postgres

--- example/main.tf
+++ example/main.tf
@@ -1,56 +1,55 @@
 module "minio" {
-  source        = "github.com/fredrikhgrelland/terraform-nomad-minio.git?ref=0.0.2"
+  source = "github.com/fredrikhgrelland/terraform-nomad-minio.git?ref=0.1.0"
 
   # nomad
   nomad_datacenters = ["dc1"]
-  nomad_namespace = "default"
+  nomad_namespace   = "default"
 
   # minio
-  service_name = "minio"
-  host = "127.0.0.1"
-  port = 9000
-  container_image = "minio/minio:latest" # todo: avoid using tag latest in future releases
-  access_key = "minio"
-  secret_key = "minio123"
-  buckets = ["default", "hive"]
+  service_name                    = "minio"
+  host                            = "127.0.0.1"
+  port                            = 9000
+  container_image                 = "minio/minio:latest" # todo: avoid using tag latest in future releases
+  access_key                      = "minio"
+  secret_key                      = "minio123"
+  buckets                         = ["default", "hive"]
   container_environment_variables = ["JUST_EXAMPLE_VAR1=some-value", "ANOTHER_EXAMPLE2=some-other-value"]
 
   # mc
-  mc_service_name = "mc"
-  mc_container_image = "minio/mc:latest" # todo: avoid using tag latest in future releases
+  mc_service_name                    = "mc"
+  mc_container_image                 = "minio/mc:latest" # todo: avoid using tag latest in future releases
   mc_container_environment_variables = ["JUST_EXAMPLE_VAR3=some-value", "ANOTHER_EXAMPLE4=some-other-value"]
 }
 
 module "postgres" {
-  source                  = "github.com/fredrikhgrelland/terraform-nomad-postgres.git?ref=0.0.1"
+  source = "github.com/fredrikhgrelland/terraform-nomad-postgres.git?ref=0.1.0"
 
   # nomad
   nomad_datacenters = ["dc1"]
-  nomad_namespace = "default"
+  nomad_namespace   = "default"
 
   # postgres
-  postgres_service_name = "postgres"
-  postgres_container_image = "postgres:12-alpine"
-  postgres_container_port = 5432
-  postgres_admin_user     = "hive"
-  postgres_admin_password = "hive"
-  postgres_database       = "metastore"
-  postgres_container_environment_variables = ["PGDATA=/var/lib/postgresql/data"]
+  service_name                    = "postgres"
+  container_image                 = "postgres:12-alpine"
+  container_port                  = 5432
+  admin_user                      = "hive"
+  admin_password                  = "hive"
+  database                        = "metastore"
+  container_environment_variables = ["PGDATA=/var/lib/postgresql/data"]
 }
 
 module "hive" {
   source = "./.."
 
   # nomad
-  nomad_datacenters = ["dc1"]
-  nomad_namespace = "default"
-
+  nomad_datacenters      = ["dc1"]
+  nomad_namespace        = "default"
   nomad_job_switch_local = false
 
   # hive
-  hive_service_name = "hive-metastore"
-  hive_container_port = 9083
-  hive_docker_image = "fredrikhgrelland/hive:3.1.0"
+  hive_service_name                    = "hive-metastore"
+  hive_container_port                  = 9083
+  hive_docker_image                    = "fredrikhgrelland/hive:3.1.0"
   hive_container_environment_variables = ["SOME_EXAMPLE=example-value"]
 
   # hive - minio
