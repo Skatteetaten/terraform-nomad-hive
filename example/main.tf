b4c36c39ced52067aed9721a2921a6d8eae18071
zhenik <nikita.zhevnitskiy@gmail.com>
2020-10-23 14:31:05 +0200

    bump minio ~> 0.2.0

--- example/main.tf
+++ example/main.tf
@@ -1,9 +1,10 @@
 module "minio" {
-  source = "github.com/fredrikhgrelland/terraform-nomad-minio.git?ref=0.1.0"
+  source = "github.com/fredrikhgrelland/terraform-nomad-minio.git?ref=0.2.0"
 
   # nomad
   nomad_datacenters = ["dc1"]
   nomad_namespace   = "default"
+  nomad_host_volume = "persistence"
 
   # minio
   service_name                    = "minio"
@@ -12,9 +13,10 @@ module "minio" {
   container_image                 = "minio/minio:latest" # todo: avoid using tag latest in future releases
   access_key                      = "minio"
   secret_key                      = "minio123"
+  data_dir                        = "/minio/data"
   buckets                         = ["default", "hive"]
   container_environment_variables = ["JUST_EXAMPLE_VAR1=some-value", "ANOTHER_EXAMPLE2=some-other-value"]
-
+  use_host_volume                 = true
   # mc
   mc_service_name                    = "mc"
   mc_container_image                 = "minio/mc:latest" # todo: avoid using tag latest in future releases
