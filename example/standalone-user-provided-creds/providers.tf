05f00efd647add997a846f873415241ece15b8a7
Per Daniel Thorsrud <PerDaniel.Thorsrud@skatteetaten.no>
2021-03-11 15:09:14 +0100

    fixed the god damn missing ACL token

--- example/standalone-user-provided-creds/providers.tf
+++ example/standalone-user-provided-creds/providers.tf
@@ -1,3 +1,8 @@
+provider "vault" {
+  address = "http://127.0.0.1:8200"
+  token   = "master"
+}
+
 data "vault_generic_secret" "nomad_secret_id" {
   # Set count of this data source to 1 if ACLs are enabled in Nomad, and 0 if not
   count = var.nomad_acl ? 1 : 0
@@ -7,12 +12,7 @@ data "vault_generic_secret" "nomad_secret_id" {
 provider "nomad" {
   address = "http://127.0.0.1:4646"
   # Add a secret_id if ACLs are enabled in nomad
-  secret_id = var.nomad_acl ? data.vault_generic_secret.nomad_secret_id.data.secret_id : null
-}
-
-provider "vault" {
-  address = "http://127.0.0.1:8200"
-  token   = "master"
+  secret_id = var.nomad_acl ? data.vault_generic_secret.nomad_secret_id[0].data.secret_id : null
 }
 
 terraform {
